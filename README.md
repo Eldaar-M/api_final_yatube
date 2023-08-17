f2a089ca09541a9d87e4a34db00daec1a8054553
Eldar Magomedov <emagomedow@yandex.ru>
2023-08-17 23:15:18 +0300

    api_final_yatube v.1

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# api_final
-api final
+# API для Yatube
+### Описание
+API позволяет взаимодействовать с постами, комментариями, группами и подписками на платформе Yatube.
+### Установка
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone https://github.com/yandex-praktikum/api_final_yatube.git
+```
+
+```
+cd api_final_yatube
+```
+
+Cоздать и активировать виртуальное окружение:
+
+```
+python3 -m venv env
+```
+
+```
+source env/bin/activate
+```
+
+Установить зависимости из файла requirements.txt:
+
+```
+python3 -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+
+```
+python3 manage.py migrate
+```
+
+Запустить проект:
+
+```
+python3 manage.py runserver
+```
+### Примеры запросов к API
+
+> GET Получение публикаций api/v1/posts/
+```200:```
+
+```sh
+{
+  "count": 123,
+  "next": "http://api.example.org/accounts/?offset=400&limit=100",
+  "previous": "http://api.example.org/accounts/?offset=200&limit=100",
+  "results": [
+    {
+      "id": 0,
+      "author": "string",
+      "text": "string",
+      "pub_date": "2021-10-14T20:41:29.648Z",
+      "image": "string",
+      "group": 0
+    }
+  ]
+}
+```
+
+> POST Создание публикации api/v1/posts/
+```200:```
+
+```sh
+{
+  "text": "string",
+  "image": "string",
+  "group": 0
+}
+```
+
+> PATCH Частичное обновление публикации api/v1/posts/{id}/
+```200:```
+
+```sh
+{
+  "text": "string",
+  "image": "string",
+  "group": 0
+}
+```
+
+```401:```
+
+```sh
+{
+  "detail": "Учетные данные не были предоставлены."
+}
+```
+
+```403:```
+
+```sh
+{
+  "detail": "У вас недостаточно прав для выполнения данного действия."
+}
+```
+
+```404:```
+
+```sh
+{
+  "detail": "Страница не найдена."
+}
+```
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Follow, Group, Post, User
 
 
-from posts.models import Comment, Post
+MESSAGE_SUB_ISSUED = 'Подписка уже оформлена'
+MESSAGE_SUB_YOURSELF = 'Подписка на самого себя невозможна'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -14,10 +18,43 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
+
+    def validate(self, data):
+        if self.context.get('request').user == data['following']:
+            raise serializers.ValidationError(MESSAGE_SUB_YOURSELF)
+        return data
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message=MESSAGE_SUB_ISSUED
+            ),
+        )
